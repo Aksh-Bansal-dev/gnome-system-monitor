54d4ecde9c01524a41c6afb50eaa694f46e55411
Aksh Bansal <akshmark@gmail.com>
2021-07-18 01:00:10 +0530

    initial commit

---
+++ extension.js
@@ -0,0 +1,65 @@
+const {St, GLib, Clutter} = imports.gi;
+const Main = imports.ui.main;
+const Mainloop = imports.mainloop;
+
+let panelButton, panelButtonText, timeout;
+
+function setButtonText () {
+    
+  var arr = [];
+  
+  // CPU
+  var [ok, out, err, exit] = GLib.spawn_command_line_sync(
+    "/bin/bash -c \"grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}'\""
+  );
+  if (out.length > 0) {
+      let cpu = parseFloat(out.toString().replace('\n', ''));
+      
+    arr.push("CPU: "+cpu.toFixed(2)+"%");
+  }
+  else{
+    arr.push("CPU");   
+  }
+  
+
+  // MEMORY
+  var [ok, out, err, exit] = GLib.spawn_command_line_sync(
+    "/bin/bash -c \"cat /proc/meminfo | awk '/MemFree/ {print $2/(1024*1024)}'\""
+  );
+  if (out.length > 0) {
+      let mem = parseFloat(out.toString().replace('\n', ''));
+      
+    arr.push("Mem: "+mem.toFixed(2)+" GB");
+  }
+  else{
+    arr.push("RAM");   
+  }
+  
+  
+  
+  panelButtonText.set_text( arr.join('      ') );
+  
+  return true;
+}
+
+function init () {
+  panelButton = new St.Bin({
+    style_class : "panel-button"
+  });
+  panelButtonText = new St.Label({
+    style_class : "examplePanelText",
+    text : "Starting ...",
+    y_align: Clutter.ActorAlign.CENTER,
+  });
+  panelButton.set_child(panelButtonText);
+}
+
+function enable () {
+  Main.panel._rightBox.insert_child_at_index(panelButton, 1);
+  timeout = Mainloop.timeout_add_seconds(1.0, setButtonText);
+}
+
+function disable () {
+  Mainloop.source_remove(timeout);
+  Main.panel._rightBox.remove_child(panelButton);
+}
