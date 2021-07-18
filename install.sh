bb76dc0de4960fe360c041432b729907829f7438
Aksh Bansal <akshmark@gmail.com>
2021-07-18 11:17:56 +0530

    add install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir ~/.local/share/gnome-shell/sysmonitor@example.com
+cp ./extension.js ~/.local/share/gnome-shell/sysmonitor@example.com/extension.js
+cp ./metadata.json ~/.local/share/gnome-shell/sysmonitor@example.com/metadata.json
