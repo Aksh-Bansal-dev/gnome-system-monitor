e779c3fb647b7f4f3e4788383522e341ba54cf18
Aksh Bansal <63552235+Aksh-Bansal-dev@users.noreply.github.com>
2021-07-18 19:09:41 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-mkdir ~/.local/share/gnome-shell/sysmonitor@example.com
-cp ./extension.js ~/.local/share/gnome-shell/sysmonitor@example.com/extension.js
-cp ./metadata.json ~/.local/share/gnome-shell/sysmonitor@example.com/metadata.json
+mkdir ~/.local/share/gnome-shell/extensions/sysmonitor@example.com
+cp ./extension.js ~/.local/share/gnome-shell/extensions/sysmonitor@example.com/extension.js
+cp ./metadata.json ~/.local/share/gnome-shell/extensions/sysmonitor@example.com/metadata.json
+gnome-extensions enable sysmonitor@example.com
